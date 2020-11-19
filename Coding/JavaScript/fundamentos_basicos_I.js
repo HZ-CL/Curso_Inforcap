739427c2613baaa8e5de2ce3311294a90cc785d3
Hector Zambrano <hzambranoleal@gmail.com>
2020-11-19 20:33:01 -0300

    fundamentos 1

---
+++ Coding/JavaScript/fundamentos_basicos_I.js
@@ -0,0 +1,45 @@
+//// ex 01 /////
+//Obtén del 1 al 255: Escribe una función que devuelve un array con todos los números del 1 al 255.
+
+function a() {
+    let array = []
+    for (i = 1; i <= 255; i++) {
+        array.push(i)
+    }
+    return array
+}
+
+console.log(a())
+
+//// ex 02 /////
+//Consigue pares hasta 1000: Escribe una función que entregue la suma de todos los números pares del 1 al 1000 - 
+//Puedes usar un operador de módulo para este ejercicio. 
+
+function sumapares() {
+    var suma = 0
+    for (i = 1; i <= 1000; i++) {
+        if (i / 2) {
+            suma = suma + 1
+        }
+    }
+    return suma
+}
+sumapares()
+
+
+//// ex 03 /////
+//Suma impares hasta 5000: Escribe una función que devuelva la suma de todos los números impares entre 1 y 5000 
+//(ej: 1+3+5+...+4997+4999).
+
+
+
+//// ex 04 /////
+//// ex 05 /////
+//// ex 06 /////
+//// ex 07 /////
+//// ex 08 /////
+//// ex 09 /////
+//// ex 10 /////
+//// ex 11 /////
+//// ex 12 /////
+//// ex 13 /////
