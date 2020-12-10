8e56f0f43f526d6532628d6f0ba8c57aaa8cd85e
Hector Zambrano <hzambranoleal@gmail.com>
2020-12-09 22:29:15 -0300

    funciones jQuery

---
+++ Coding/JQuery/Comandos_jQuery.js
@@ -0,0 +1,61 @@
+/* Lista de funciones con las que deberías familiarizarte:
+
+Efectos (funciones para hacer algunos efectos de animación geniales) 
+    .hide() *para ocultar*
+    .show() *para mostrar*
+    .toggle() *para ocultar y luego mostrar contenido*
+    .slideUp() *para ocultar con animacion de deslizar hacia arriba*
+    .slideDown() *para visualizar contenido con animacion de despliege hacia abajo*
+    .slideToggle() * para oculatr y mostrar contenido*
+    .fadeOut() *quitar elementos con animacion desvanecimiento*
+    .fadeIn() *muestra elementos trasparebtes a opacos*
+
+
+    CSS (agregar o quitar una clase para cualquier elemento/DOM de HTML)
+    .addClass() *agregar clases en las etiquetas*
+    .removeClass() *elimina etiquetas
+    .css() *agrega estilos de css al elementos*
+
+
+Manipulación (recuperar o configurar un valor o texto en cualquier elemento de HTML) 
+    .after() *agrega contenido despues de la etiqueta mencionada*
+    .append() *agrega dentro y al final del elemento mencionado*
+    .prepend() *agrega dentro y al inicio del elemento mencionado*
+    .attr() *muestra lo que esta almacenado en elemento mencionado*
+    .before() *agrega contenido antes de la etiqueta mencionada*
+    .html() *estilos para html*
+    .text() *para modificar el text*
+    .val() *establece el valor del elemento*
+
+
+Eventos (funciones para manejar un evento)
+    .click() *condiciona una funcion para cuando se haga un click*
+    .hover() *para cuando el mouse pase por el contenido, puede tener dos funciones*
+    .on()
+    .live()
+
+    */
+
+
+/* $('etiqueta ,class) el espacio entre la etiqueta y la coma de la clase es para seleccionar la clase dentro de la etiqueta*/
+// $('').clic(function() {$(this).toggle();});
+
+
+
+//SCRIPT PARA INSCORPORAR EN HEAD DE HTML
+
+//</head><script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" > < /script> 
+
+
+
+//PARA ACTUALIZAR BIBLIOTECA 
+
+//https://developers.google.com/speed/libraries/#jquery 
+
+
+//FINCION PARA INICIO DE jQuery
+
+$(document).ready(function() {
+
+
+});
